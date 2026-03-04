542fb46598a74722297fcedd292acef46c4d4b5a
Blivyy <patrick.murphy@magairports.com>
2026-03-04 16:30:45 +0000

    Add second app

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/C-A-Dashboard-Version-1.0/",
+  base: "/C-A---Dashboard-Test-Centre/",
   plugins: [react()],
 });
